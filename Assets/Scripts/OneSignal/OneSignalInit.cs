15c3c1faf1a87d66b87c63f0451cf6470ef2ab96
Furion <artem-furlietov@meta.ua>
2020-12-25 13:39:22 +0200

    one signal edit

--- Assets/Scripts/OneSignal/OneSignalInit.cs
+++ Assets/Scripts/OneSignal/OneSignalInit.cs
@@ -2,13 +2,13 @@
 
 public class OneSignalInit: MonoBehaviour
 {
-    private const string AppId = "d9a15521-0f6d-4cb5-b561-dfe3db257518";
+    public string appId = "d9a15521-0f6d-4cb5-b561-dfe3db257518";
     private void Start()
     {
         // Uncomment this method to enable OneSignal Debugging log output 
-        OneSignal.SetLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+        //OneSignal.SetLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
   
-        OneSignal.StartInit(AppId)
+        OneSignal.StartInit(appId)
             .EndInit();
         
         OneSignal.inFocusDisplayType = OneSignal.OSInFocusDisplayOption.Notification;
