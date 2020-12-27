40157bd6aeb3254dedb320b1af3378468e52d295
Furion <artem-furlietov@meta.ua>
2020-12-27 20:13:14 +0200

    facebook installed

---
+++ Assets/Scripts/FacebookInit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Facebook.Unity;
+using Facebook.Unity.Example;
+using UnityEngine;
+
+public class FacebookInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start()
+    {
+        FB.Init(OnInitComplete);
+    }
+    private void OnInitComplete()
+    {
+        var logMessage = $"OnInitCompleteCalled IsLoggedIn='{FB.IsLoggedIn}' IsInitialized='{FB.IsInitialized}'";
+        LogView.AddLog(logMessage);
+        if (AccessToken.CurrentAccessToken != null)
+        {
+            LogView.AddLog(AccessToken.CurrentAccessToken.ToString());
+        }
+    }
+
+}
